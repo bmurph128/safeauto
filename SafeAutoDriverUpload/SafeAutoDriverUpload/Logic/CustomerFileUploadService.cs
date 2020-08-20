3de0dfc687b98e28ab7d429186c5c5e83bd63d1d
Brandon Murphy <bmurph128@gmail.com>
2020-08-20 12:36:18 -0400

    added checks for trip mph limitations

--- SafeAutoDriverUpload/SafeAutoDriverUpload/Logic/CustomerFileUploadService.cs
+++ SafeAutoDriverUpload/SafeAutoDriverUpload/Logic/CustomerFileUploadService.cs
@@ -8,89 +8,95 @@ using System.Threading.Tasks;
 
 namespace SafeAutoDriverUpload.Logic
 {
-  public class CustomerFileUploadService : ICustomerFileUploadService
-  {
-    
-    private List<Driver> ReadFile(IFormFile file)
+    public class CustomerFileUploadService : ICustomerFileUploadService
     {
-      List<Driver> drivers = new List<Driver>();
 
-      try
-      {
-        using(var sr = new StreamReader(file.OpenReadStream()))
+        private List<Driver> ReadFile(IFormFile file)
         {
-          while (!sr.EndOfStream)
-          {
-            string unsplit = sr.ReadLine();
-            string[] split = unsplit.Split(' ');
+            List<Driver> drivers = new List<Driver>();
 
-            if (split[0] == "Driver")
+            try
             {
-              Driver tempDriver = new Driver();
-              tempDriver.Command = split[0];
-              tempDriver.Name = split[1];
-              drivers.Add(tempDriver);
-            }
-            else if (split[0] == "Trip")
-            {
-              Trip tempTrip = new Trip();
-              tempTrip.Command = split[0];
-              tempTrip.Name = split[1];
-              tempTrip.TripStartTime = DateTime.Parse(split[2]);
-              tempTrip.TripEndTime = DateTime.Parse(split[3]);
-              tempTrip.MilesDriven = double.Parse(split[4]);
-
-              foreach (Driver driver in drivers)
-              {
-                if (tempTrip.Name == driver.Name)
+                using (var sr = new StreamReader(file.OpenReadStream()))
                 {
-                  driver.Trips.Add(tempTrip);
+                    while (!sr.EndOfStream)
+                    {
+                        string unsplit = sr.ReadLine();
+                        string[] split = unsplit.Split(' ');
+
+                        if (split[0] == "Driver")
+                        {
+                            Driver tempDriver = new Driver();
+                            tempDriver.Command = split[0];
+                            tempDriver.Name = split[1];
+                            drivers.Add(tempDriver);
+                        }
+                        else if (split[0] == "Trip")
+                        {
+                            Trip tempTrip = new Trip();
+                            tempTrip.Command = split[0];
+                            tempTrip.Name = split[1];
+                            tempTrip.TripStartTime = DateTime.Parse(split[2]);
+                            tempTrip.TripEndTime = DateTime.Parse(split[3]);
+                            tempTrip.MilesDriven = double.Parse(split[4]);
+
+                            foreach (Driver driver in drivers)
+                            {
+                                if (tempTrip.Name == driver.Name)
+                                {
+                                    driver.Trips.Add(tempTrip);
+                                }
+                            }
+                        }
+                    }
                 }
-              }
             }
-          }
+            catch
+            {
+                drivers = new List<Driver>();
+            }
+
+            return drivers;
         }
-      }
-      catch
-      {
-        drivers = new List<Driver>();
-      }
 
-      return drivers;
-    }
-
-    public List<DriverDto> UploadFile(IFormFile file)
-    {
-      var drivers = ReadFile(file);
-
-      var dtos = new List<DriverDto>();
-
-
-
-      foreach (Driver driver in drivers)
-      {
-        var dto = new DriverDto();
-        double minutes = 0;
-        double miles = 0;
-
-        foreach (Trip data in driver.Trips)
+        public List<DriverDto> UploadFile(IFormFile file)
         {
-          miles = miles + data.MilesDriven;
-          TimeSpan timeDriving = data.TripEndTime - data.TripStartTime;
-          minutes += timeDriving.TotalMinutes;
+            var drivers = ReadFile(file);
 
+            var dtos = new List<DriverDto>();
+
+
+
+            foreach (Driver driver in drivers)
+            {
+                var dto = new DriverDto();
+                double hours = 0;
+                double miles = 0;
+                double totalMiles = 0;
+                double totalHours = 0;
+
+                foreach (Trip data in driver.Trips)
+                {
+                    TimeSpan timeDriving = data.TripEndTime - data.TripStartTime;
+                    hours = timeDriving.TotalMinutes / 60;
+                    if (data.MilesDriven / hours >= 5 && data.MilesDriven / hours <= 100)
+                    {
+                        totalMiles += data.MilesDriven;
+                        totalHours += hours;
+                    }
+
+                }
+
+                double mph = totalHours > 0 ? totalMiles / totalHours : 0;
+                double roundedMph = Math.Round(mph);
+                double roundedMiles = Math.Round(totalMiles);
+                dto.Name = driver.Name;
+                dto.Miles = roundedMiles;
+                dto.Mph = roundedMph;
+                dtos.Add(dto);
+            }
+
+            return dtos;
         }
-
-        double mph = minutes > 0 ? miles / (minutes / 60) : 0;
-        double roundedMph = Math.Round(mph);
-        double roundedMiles = Math.Round(miles);
-        dto.Name = driver.Name;
-        dto.Miles = roundedMiles;
-        dto.Mph = roundedMph;
-        dtos.Add(dto);
-      }
-
-      return dtos;
     }
-  }
 }
